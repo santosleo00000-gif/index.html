357d4503f0365a53ed1e30fab9632bd22f5c26fe
santosleo00000-gif <santosleo00000@gmail.com>
2026-07-22 23:48:15 -0300

    Delete bly-automation 2 directory

--- bly-automation 2/src/index.local.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-const { startAllJobs } = require('./jobs/scheduler');
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`[Server] BLY! Automation rodando na porta ${PORT} (modo local)`);
-  startAllJobs(); // node-cron só funciona em processo contínuo (uso local / VPS)
-});
